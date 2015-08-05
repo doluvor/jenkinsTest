49bad5d735e8faf37c77b2aa0c4cd1d00abd5414
doluvor <doluvor@126.com>
2015-08-05 11:38:26 +0800

    init project.

---
+++ jenkinsTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  jenkinsTest
+//
+//  Created by iosDevMacbookPro on 15/8/3.
+//  Copyright (c) 2015年 doluvor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var errorArray = [1, 2]
+        
+        var i = errorArray[3]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
